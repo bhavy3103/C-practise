33cdd29763bd581081926d06bd97e96b8d3c04e4
Bhavy3103 <105499961+Bhavy3103@users.noreply.github.com>
2022-05-16 20:51:38 +0530

    Add files via upload

---
+++ sizeof.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+   // Find the size of all the datatypes in bytes.
+int main()
+{
+    cout << " Size of char is " << sizeof(char) << " byte" << endl ;
+    cout << " Size of int is " << sizeof(int) << " bytes" << endl ;
+    cout << " Size of float is " << sizeof(float) << " bytes" << endl ;
+    cout << " Size of double is " << sizeof(double) << " bytes" << endl ;
+    cout << " Size of long int is " << sizeof(long int) << " bytes" << endl ;
+    cout << " Size of long long int is " << sizeof(long long int) << " bytes" << endl ;
+    cout << " Size of long double is " << sizeof( long double) << " bytes" << endl ;
+
+
+   return 0;
+}
