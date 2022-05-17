e6d1ee4d3996a22700604fad3151ef34ebb3dde9
Bhavy3103 <105499961+Bhavy3103@users.noreply.github.com>
2022-05-17 18:20:26 +0530

    Add files via upload

---
+++ Area_triangle.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// Calculate area for right angle triangle .
+
+
+int main()
+{
+    float h,b,a;  
+
+    // h=height , b=base , a=area 
+
+    cout << "Enter the Value of height and base respectively" ;
+
+    cin >> h >> b ;
+
+
+    a = ( h * b ) / 2.0 ;
+   
+    cout << "Area of triangle is = " << a ;
+
+  return 0;
+  
+}
