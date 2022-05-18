0c6bb41518c5ea9e59a3bee253dbe43ae23acd01
Bhavy3103 <105499961+Bhavy3103@users.noreply.github.com>
2022-05-18 17:22:58 +0530

    Add files via upload

---
+++ maxof2.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n1,n2,max ;
+
+    cout << "Enter any two integer number here : " ;
+    cin >> n1 >> n2 ;
+
+    max= (n1 > n2) ? n1 : n2 ;
+
+    cout << "Maximum Number is = " << max ;
+
+  return 0;
+}
