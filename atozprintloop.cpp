96983eca7824f3aaee3ddb09a7ff33d23101e5ad
Bhavy3103 <105499961+Bhavy3103@users.noreply.github.com>
2022-05-24 19:04:08 +0530

    Add files via upload

---
+++ atozprintloop.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+     // Uppercase Alphabet 
+    for ( char c='A' ; c<='Z+1' ; c++)
+    {
+        cout << c << " " ;
+    }
+    cout << endl ;
+
+    // Lower case Alphabet
+    for ( char c='a' ; c<='z' ; c++)
+    {
+        cout << c << " " ;
+    }
+
+  return 0;
+}
