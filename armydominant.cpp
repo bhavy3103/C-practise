9b0fcf86892bfdb669ad33f1a67aca3eb3857000
Bhavy3103 <105499961+Bhavy3103@users.noreply.github.com>
2022-06-04 22:35:30 +0530

    Add files via upload

---
+++ armydominant.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t, a, b, c;
+    cin >> t;
+    for (int i = 1; i <= t; i++)
+    {
+        cin >> a >> b >> c;
+        if (a > (b + c) || b > (a + c) || c > (a + b))
+        {
+            cout << "YES" << endl;
+        }
+        else
+        {
+            cout << "NO" << endl;
+        }
+    }
+    return 0;
+}
